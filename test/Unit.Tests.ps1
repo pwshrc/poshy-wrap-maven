ed52d062f258e6fb0c7aa113714bcbd0971c2bd2
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-08-27 16:00:42 -0400

    ✅ Updates Rudimentary Unit Test

--- test/Unit.Tests.ps1
+++ test/Unit.Tests.ps1
@@ -4,10 +4,19 @@ $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
 
+
+BeforeAll {
+    . "$PSScriptRoot/_BeforeAll.ps1"
+}
+
 Describe "module" {
     It "should be importable" {
         [PSModuleInfo] $module = Import-Module $Global:SubjectModule -Force -DisableNameChecking -NoClobber -PassThru
 
         $module | Should -Not -BeNull
     }
+}
+
+AfterAll {
+    . "$PSScriptRoot/_AfterAll.ps1"
 }
