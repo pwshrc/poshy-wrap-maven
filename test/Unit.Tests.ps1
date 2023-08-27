880c24401eeeab77c478bb85ae9dd10023dfb3cd
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-08-27 10:55:16 -0400

    👷 Updates Rudimentary Unit Test

--- test/Unit.Tests.ps1
+++ test/Unit.Tests.ps1
@@ -1,11 +1,12 @@
 #!/usr/bin/env pwsh
+#Requires -Modules "Pester"
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
 
 Describe "module" {
     It "should be importable" {
-        [PSModuleInfo] $module = Import-Module $Global:SubjectModule -Force -DisableNameChecking -PassThru
+        [PSModuleInfo] $module = Import-Module $Global:SubjectModule -Force -DisableNameChecking -NoClobber -PassThru
 
         $module | Should -Not -BeNull
     }
